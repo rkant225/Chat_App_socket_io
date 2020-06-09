c5c4c9fb3ba2266ea7c60e7d6b30fb81585e4048
Rahul Singh <rkant225@gmail.com>
2020-06-09 12:38:56 +0530

    assded bg2 for dummy chat mock screen

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const router = require('./Router');
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -52,6 +53,7 @@ io.on('connection', (socket)=>{
 })
 
 app.use(router);
+app.use(cors());
 
 
 server.listen(PORT, ()=>console.log(`Server started listening at port : ${PORT}`));
